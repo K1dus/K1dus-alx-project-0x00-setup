f478ac35bdb9b692487d79bb9b7f7f280f060076
K1dus <kidus.efr@gmail.com>
2024-12-21 18:47:43 +0300

    Using Components

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,7 +1,13 @@
-const Landing: React.FC = () => {
+import React from "react";
+import Card from "@/components/Card";
+
+const Landing = () => {
   return (
     <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <h1>Welcome to the Landing Page</h1>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
